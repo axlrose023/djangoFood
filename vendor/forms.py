c86d510626f00ccbb9717b7aee2559c8ac7f1780
axlrose0023 <sloboda282@gmail.com>
2022-12-08 11:27:59 +0200

    address form implementation

--- vendor/forms.py
+++ vendor/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+
+from accounts.validators import allow_only_images_validator
 from vendor.models import Vendor
 
 
 class VendorForm(forms.ModelForm):
+    vendor_license = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}),
+                                      validators=[allow_only_images_validator])
+
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
